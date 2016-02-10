5b0bbdaf2432fd6f5df2441e5ad6cbe58b72afd1
damanfb <daman.bareiss@utah.edu>
2016-02-10 14:59:57 +0000

    working with mavros

--- src/darc_rc_fly_node.cpp
+++ src/darc_rc_fly_node.cpp
@@ -1,10 +1,14 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include <roscopter/RC.h>
+#include <mavros_msgs/RCIn.h>
+#include <iostream>
 
-roscopter::RC rc_in;
-void rc_callback(const roscopter::RC& rc_msg_in) { 
-  rc_in = rc_msg_in;
+float roll, pitch, yaw , thrust;
+void rc_callback(const mavros_msgs::RCIn& rc_msg_in) { 
+  roll   = rc_msg_in.channels[0];
+  pitch  = rc_msg_in.channels[1];
+  yaw    = rc_msg_in.channels[3];
+  thrust = rc_msg_in.channels[2];
 }  // rc_callback
 
 int main(int argc, char* argv[]) {
@@ -13,18 +17,41 @@ int main(int argc, char* argv[]) {
   ros::Rate loop_rate(500);
 
   ros::Subscriber rc_sub;
-  rc_sub = node.subscribe("rc",1,rc_callback);
+  rc_sub = node.subscribe("/mavros/rc/in",1,rc_callback);
 
   ros::Publisher desired_u_pub;
   desired_u_pub = node.advertise<geometry_msgs::Twist>("desired_u",1);
 
   geometry_msgs::Twist desired_u;
+  float max_range = 1900.0, min_range = 1100.0;
+
   while (ros::ok()) {
     ros::spinOnce();
-    desired_u.linear.z = rc_in.channel[2];
-    desired_u.angular.x = rc_in.channel[0];
-    desired_u.angular.y = rc_in.channel[1];
-    desired_u.angular.z = rc_in.channel[3];
+
+    desired_u.linear.z  = (2.0 * (thrust - min_range) / (max_range - min_range)) - 1;
+    if (desired_u.linear.z < -1.0)
+      desired_u.linear.z = -1.0;
+    else if (desired_u.linear.z > 1.0)
+      desired_u.linear.z = 1.0;
+
+    desired_u.angular.x = -1.0* (2.0 * (max_range - roll) / (max_range - min_range) - 1);
+    if (desired_u.angular.x < -1.0)
+      desired_u.angular.x = -1.0;
+    else if (desired_u.angular.x > 1.0)
+      desired_u.angular.x = 1.0;
+
+    desired_u.angular.y = -1.0*(1 - 2.0 * (max_range - pitch) / (max_range - min_range));
+    if (desired_u.angular.y < -1.0)
+      desired_u.angular.y = -1.0;
+    else if (desired_u.angular.y > 1.0)
+      desired_u.angular.y = 1.0;
+
+    desired_u.angular.z = 2.0*(max_range - yaw) / (max_range - min_range) - 1;
+    if (desired_u.angular.z < -1.0)
+      desired_u.angular.z = -1.0;
+    else if (desired_u.angular.z > 1.0)
+      desired_u.angular.z = 1.0;
+
     desired_u_pub.publish(desired_u);
     loop_rate.sleep();
   }
